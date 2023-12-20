887c50b1b2930b0dcbaac5844d20f63378a43827
VeryLittleAnna <very.little.anna@gmail.com>
2023-12-20 16:14:46 +0300

    Добавлена возможность восстановления весов лучшей модели

--- Utils.py
+++ Utils.py
@@ -17,7 +17,7 @@ class RMSELoss(nn.Module):
         return torch.sqrt(self.mse(output, target))
 
 class EarlyStopper:
-    def __init__(self, patience=1, min_delta=0, path=None):
+    def __init__(self, patience=1, min_delta=0, start_from_epoch=0, path=None):
         self.patience = patience
         self.min_delta = min_delta
         self.counter = 0
@@ -25,20 +25,27 @@ class EarlyStopper:
         self.path = path
         self.min_loss_epoch_number = -1
         self.counter_not_better = 0
+        self.start_from_epoch = start_from_epoch
 
     def early_stop(self, validation_loss, model=None):
-        self.counter += 1
+        self.counter += 1   
         if validation_loss < self.min_validation_loss:
             self.min_validation_loss = validation_loss
             self.min_loss_epoch_number = self.counter
             self.counter_not_better = 0
             if self.path is not None and model is not None:
                 torch.save(model.state_dict(), self.path)
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
+        elif validation_loss > (self.min_validation_loss + self.min_delta) and self.counter > self.start_from_epoch:
             self.counter_not_better += 1
             if self.counter_not_better >= self.patience:
                 return True
         return False
+    
+    def restore_model(self, model):
+        if self.path is None:
+            raise("Best model haven't saved, no path")
+        model.load_state_dict(torch.load(self.path))
+        return model
     
 def MAPE(target, output):
     target = np.maximum(np.abs(np.array(target)), 1e-15)
