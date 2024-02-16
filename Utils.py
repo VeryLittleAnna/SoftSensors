bdbfe8c122ad2b875f839ced25fa22aa6a60d83d
VeryLittleAnna <very.little.anna@gmail.com>
2024-02-16 19:45:29 +0300

    Вынесена фиксация seed

--- Utils.py
+++ Utils.py
@@ -54,3 +54,14 @@ def MAPE(target, output):
     output = np.array(output)
     mape = np.mean(np.abs(target - output) / target, axis=-1)
     return mape
+
+def global_seed(seed: int) -> None:
+    """
+    Set global seed for reproducibility.
+    """
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
